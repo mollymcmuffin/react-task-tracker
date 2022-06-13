adda1bb6acc52f0052b4d578c662b6c7210de5b2
zhengxuan <102131532+mollymcmuffin@users.noreply.github.com>
2022-06-13 16:02:01 +0800

    Set up of React
    Set up the relevant components.

---
+++ react-task-tracker/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Button from './Button'
+
+const Header = ({title}) => {
+  
+  const whenClick = () =>{
+      console.log('Click!')
+  }
+
+  return (
+    <header className = 'header'>
+        <h1>{title}</h1>
+        <Button text = 'Add' color = 'green' whenClick = {whenClick}/>
+    </header>
+  )
+}
+
+export default Header
