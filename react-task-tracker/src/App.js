8a39a08a55ba8461d68f1d23be2deb505da5a3a2
zhengxuan <102131532+mollymcmuffin@users.noreply.github.com>
2022-06-14 13:22:01 +0800

    Added toggle on task

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -23,10 +23,18 @@ function App() {
         reminder: false
       }])
   
+  const deleteTask = (id) =>{
+    setTasks(tasks.filter((task)=> task.id !== id)) 
+  };
+
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task)=> task.id === id ? {...task, reminder:!task.reminder} :task))
+  };
+
   return (
     <div className="container">
       <Header  title = 'Task Tracker'/>
-      <Tasks tasks = {tasks}/>
+      {tasks.length > 0 ? (<Tasks tasks = {tasks} onDelete = {deleteTask} onToggle = {toggleReminder}/>): 'No tasks'}
     </div>
   );
 }
